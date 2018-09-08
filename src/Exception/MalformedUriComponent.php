8e9212eea58f851762c3195a1844c940c14de7dc
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2018-09-08 10:48:05 +0200

    remove remaining uri-components links

--- src/Exception/MalformedUriComponent.php
+++ src/Exception/MalformedUriComponent.php
@@ -4,7 +4,7 @@
  * League.Uri (http://uri.thephpleague.com).
  *
  * @author  Ignace Nyamagana Butera <nyamsprod@gmail.com>
- * @license https://github.com/thephpleague/uri-components/blob/master/LICENSE (MIT License)
+ * @license https://github.com/thephpleague/uri-query-parser/blob/master/LICENSE (MIT License)
  * @version 1.0.0
  * @link    https://github.com/thephpleague/uri-query-parser
  *
